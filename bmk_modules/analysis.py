9a3f96fc4e9ef470f2054dde1149defaa7cb9201
Alessandro Palma <alessandro@awareserver.diag.uniroma1.it>
2024-02-07 14:52:44 +0100

    cost analysis

--- bmk_modules/analysis.py
+++ bmk_modules/analysis.py
@@ -9,6 +9,48 @@ output_cleanfolder=config.output_cleanfolder
 colors_models=config.colors_models
 plot_folder=config.plot_folder
 
+def plot_costs():
+    cost_df = pd.DataFrame()
+
+    for file_log in os.listdir(output_cleanfolder):
+        if "rank" in file_log or "metric" in file_log: continue
+        df_log = pd.read_csv(output_cleanfolder+file_log)
+        if len(cost_df)==0: cost_df=df_log
+        else: cost_df = pd.concat([cost_df, df_log], ignore_index=True)
+    
+    fig = plt.figure(figsize=(5, 3))
+    plt.rcParams.update({'font.size': 14})
+
+    models = []
+    for col in cost_df.columns: 
+        if "gt" in col: models.append(col)    
+    models.sort()
+
+
+    x_position_increment = 1 / len(models)
+    models_labels=[]
+    for i in range(0,len(models)):
+        positions = []
+        positions.append(i)# + (x_position_increment*i))
+
+        values = cost_df[models[i]].to_list()
+        norm_val = [float(i)/max(values) for i in values]
+        plt.boxplot(norm_val, positions=positions, notch=False, patch_artist=True,
+                    boxprops=dict(facecolor=colors_models[i], color="#000000"),
+                    capprops=dict(color="#000000"),
+                    whiskerprops=dict(color="#000000"),
+                    flierprops=dict(color="#000000",
+                                    markeredgecolor="#000000"),
+                    widths=x_position_increment * 2
+                    )
+        models_labels.append(models[i].replace("gt","M"))
+            
+    plt.xlabel("models")
+    plt.ylabel("incidents cost")
+    plt.xticks(range(0,len(models)), models_labels)
+    plt.savefig(plot_folder+"cost_box.png", bbox_inches='tight')
+    return
+
 def plot_performance():
     rank_df = pd.DataFrame()
 
@@ -21,7 +63,7 @@ def plot_performance():
                     if len(rank_df)==0: rank_df=metric_df
                     else: rank_df = pd.concat([rank_df, metric_df], ignore_index=True)
 
-    fig = plt.figure(figsize=(6, 5))
+    fig = plt.figure(figsize=(5, 3))
     plt.rcParams.update({'font.size': 14})
 
     models = list(set((rank_df["Model"].to_list()+rank_df["Gt"].to_list())))
@@ -30,7 +72,6 @@ def plot_performance():
     x_position_increment = 1 / len(models)
     models_labels=[]
     for i in range(0,len(models)):
-
         positions = []
         positions.append(i)# + (x_position_increment*i))
 
@@ -56,4 +97,5 @@ def plot_performance():
 
 
 def main_analysis():
+    plot_costs()
     plot_performance()
